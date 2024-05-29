c7883761d4dac52665cc45d8cac55514b79a3dd7
NumericFactory <frederic.lossignol@gmail.com>
2024-05-29 18:47:15 +0200

    fix : store images folder in public/assets/images

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@ import '../css/home-expertises.css';
 import '../css/home-onboarding.css';
 import '../css/home-trustcustomers.css';
 import.meta.glob([
-    '../images/**',
-    '../fonts/**',
+    '../images/',
+    '../fonts/',
 ]);
 console.log('Hello World');
