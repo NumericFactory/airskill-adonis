9ac38c23971b952a1fa01bd6e84d7be86093a2da
NumericFactory <frederic.lossignol@gmail.com>
2024-05-29 14:35:50 +0200

    first commit

---
+++ app/controllers/posts_controller.ts
@@ -0,0 +1,36 @@
+// import type { HttpContext } from '@adonisjs/core/http'
+
+import Post from "#models/post"
+
+export default class PostsController {
+
+    public async index({ view }: any) {
+        const posts = await Post.query().preload('user')
+        console.log(posts)
+        return view.render('pages/blog', { posts })
+    }
+
+    // public async show({ view }) {
+    //     return view.render('posts/show')
+    // }
+
+    // public async create({ view }) {
+    //     return view.render('posts/create')
+    // }
+
+    // public async store({ response }) {
+    //     response.redirect().toRoute('posts.index')
+    // }
+
+    // public async edit({ view }) {
+    //     return view.render('posts/edit')
+    // }
+
+    // public async update({ response }) {
+    //     response.redirect().toRoute('posts.index')
+    // }
+
+    // public async destroy({ response }) {
+    //     response.redirect().toRoute('posts.index')
+    // }
+}
