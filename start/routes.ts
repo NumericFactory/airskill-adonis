69365e3b23249adf2a7e92a7e16a3704b70a5dfc
NumericFactory <frederic.lossignol@gmail.com>
2024-08-24 00:02:21 +0200

    add: mentions legales

--- start/routes.ts
+++ start/routes.ts
@@ -10,4 +10,5 @@
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+router.on('mentions-legales').render('pages/legal')
 router.get('blog', '#controllers/posts_controller.index')
