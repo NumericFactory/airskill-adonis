bb88b6557b0d8c292716a2377de5437d11421f89
NumericFactory <frederic.lossignol@gmail.com>
2024-11-28 12:41:37 +0100

    CSS

--- resources/js/course.js
+++ resources/js/course.js
@@ -33,7 +33,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 module.classList.remove('active');
                 module.querySelector('i').classList.remove('bi-dash');
                 module.querySelector('i').classList.add('bi-plus');
-                module.querySelector('i').style.color = '#b4b4b4';
             }
             let el = this;
             let panel = this.nextElementSibling;
@@ -41,7 +40,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 this.classList.remove('active');
                 this.querySelector('i').classList.remove('bi-dash');
                 this.querySelector('i').classList.add('bi-plus');
-                this.querySelector('i').style.color = '#b4b4b4';
                 setTimeout(function () { el.style.background = 'rgb(147 150 161 / 16%)'; }, 1000)
                 panel.style.maxHeight = null;
             } else {
@@ -51,7 +49,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 this.classList.add('active');
                 this.querySelector('i').classList.remove('bi-plus');
                 this.querySelector('i').classList.add('bi-dash');
-                this.querySelector('i').style.color = '#e91e63'
                 panel.style.maxHeight = panel.scrollHeight + "px";
             }
         })
